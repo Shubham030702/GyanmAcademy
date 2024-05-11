37469fdb21c8e317f698ce4b16c3482b0f7da9fa
Shubham030702 <dangwalshubham10@gmail.com>
2024-05-11 23:58:09 +0530

    new css modified

--- src/components/Faculty.js
+++ src/components/Faculty.js
@@ -1,47 +1,54 @@
 import React from 'react'
 import './css/Faculty.css'
 function Faculty() {
-  return (
-    <>
-    <div className="faculty">
-        <div className="top">
-            <h1>Faculty</h1>
-        </div>
-        <div className="information">
-            <div className="left">
+    return (
+        <>
+            <div className="faculty">
+                <div className="information i1">
+                    <div className="left">
 
-            </div>
-            <div className="right">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde facere ut excepturi consequuntur! Ullam praesentium error odio hic fuga odit distinctio quidem nesciunt ab unde sapiente animi labore, libero voluptatum. Praesentium enim exercitationem amet dolor.
-            </div>
-        </div>
-        <div className="information">
-            <div className="left">
+                    </div>
+                    <div className="right">
+                        <b>Faculty Name:</b> <span>Amit Uniyal</span><br />
+                        <b>Teaches:</b> <span>Mathematics</span><br />
+                        <b>Higher Qualifications:</b> <span>M.Ed & M.Sc(Mathematics)</span><br />
+                        <b>Experience:</b> <span>5+ Years of Teaching Experience.</span><br />
+                    </div>
+                </div>
+                <div className="information i2">
+                    <div className="left">
 
-            </div>
-            <div className="right">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde facere ut excepturi consequuntur! Ullam praesentium error odio hic fuga odit distinctio quidem nesciunt ab unde sapiente animi labore, libero voluptatum. Praesentium enim exercitationem amet dolor.
-            </div>
-        </div>
-        <div className="information">
-            <div className="left">
+                    </div>
+                    <div className="right">
+                        <b>Faculty Name:</b> <span>Sunil Pundir</span><br />
+                        <b>Teaches:</b> <span>Mathematics</span><br />
+                        <b>Higher Qualifications:</b> <span>M.Sc(Mathematics)</span><br />
+                        <b>Experience:</b> <span>5+ Years of Teaching Experience.</span><br />
+                    </div>
+                </div>
+                <div className="information i3">
+                    <div className="left">
 
-            </div>
-            <div className="right">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde facere ut excepturi consequuntur! Ullam praesentium error odio hic fuga odit distinctio quidem nesciunt ab unde sapiente animi labore, libero voluptatum. Praesentium enim exercitationem amet dolor.
-            </div>
-        </div>
-        <div className="information">
-            <div className="left">
+                    </div>
+                    <div className="right">
+                        <b>Faculty Name:</b> <span>Darshan Puri</span><br />
+                        <b>Teaches:</b> <span>Chemistry</span><br />
+                        <b>Higher Qualifications:</b> <span>M.Ed & M.Sc(Chemistry)</span><br />
+                        <b>Experience:</b> <span>5+ Years of Teaching Experience.</span><br />            </div>
+                </div>
+                <div className="information i4">
+                    <div className="left">
 
+                    </div>
+                    <div className="right">
+                        <b>Faculty Name:</b> <span>Ankit Jugran</span><br />
+                        <b>Teaches:</b> <span>Physics</span><br />
+                        <b>Higher Qualifications:</b> <span>M.Sc(Physics)</span><br />
+                        <b>Experience:</b> <span>6+ Years of Teaching Experience.</span><br />            </div>
+                </div>
             </div>
-            <div className="right">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde facere ut excepturi consequuntur! Ullam praesentium error odio hic fuga odit distinctio quidem nesciunt ab unde sapiente animi labore, libero voluptatum. Praesentium enim exercitationem amet dolor.
-            </div>
-        </div>
-    </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Faculty
