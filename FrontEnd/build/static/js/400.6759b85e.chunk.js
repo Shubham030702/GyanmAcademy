d1e7d89074c7fc0a0f3a1605ca9d7bb0016d5a40
Shubham030702 <dangwalshubham10@gmail.com>
2024-09-01 18:13:59 +0530

    build uploaded

---
+++ FrontEnd/build/static/js/400.6759b85e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[400],{400:function(p,e,u){u.r(e),e.default={}}}]);
+//# sourceMappingURL=400.6759b85e.chunk.js.map
---
+++ FrontEnd/build/static/js/407.1fae5f59.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[407],{407:function(p,e,u){u.r(e),e.default={}}}]);
+//# sourceMappingURL=407.1fae5f59.chunk.js.map
