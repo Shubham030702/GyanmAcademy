4a43a429b9a07896accef567ab917151245e2f77
Shubham030702 <dangwalshubham10@gmail.com>
2025-06-30 09:54:24 +0530

    Backend is changed a little bit

--- FrontEnd/src/components/Loader.js
+++ FrontEnd/src/components/Loader.js
@@ -1,7 +1,5 @@
 
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHandsPraying } from "@fortawesome/free-solid-svg-icons";
 import "./css/Loader.css"; 
 
 function Loader() {
@@ -10,7 +8,6 @@ function Loader() {
       <div className="loader"></div>
       <h1 className="waiting">
         Please Wait...{" "}
-        <FontAwesomeIcon icon={faHandsPraying} size="xl" />
       </h1>
     </>
   );
